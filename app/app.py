671245a6777644908b86a9e271298f81c4f8868b
JumaKahiga <kabirumwangi@gmail.com>
2018-11-19 15:14:09 +0300

    [Feature #162050330] Create comment

--- app/app.py
+++ app/app.py
@@ -1,4 +1,9 @@
+import datetime
+now = datetime.datetime.now()
+
 users_list = [{"username": "Harun", "password": "sensei", "role": "admin"}, {"username": "Ken", "password": "mwangi", "role": "moderator"}, {"username": "Mambo", "password": "bryan", "role": "normal"}]
+comments = []
+initial_message = "Are you happy?"
 
 class UserLogin():
 	def login_func(self):
@@ -14,3 +19,29 @@ class UserLogin():
 			else:
 				print ("Log in Unsuccessful")
 
+
+class Comments(UserLogin):
+	def __init__(self):
+		self.start = UserLogin.login_func(self)
+		#self.username = login_func
+
+	def start_message(self):
+		if self.start:
+			print(initial_message)
+			return True
+
+	def new_comment(self):
+		app_starting = Comments.start_message(self)
+		if app_starting:
+			author = input("Enter username: ")
+			comment_id = input("Enter password: ")
+			message = input("Enter message: ")    
+
+			comment = {"author": author,
+					"comment_id": len(comments)+1,
+					"comment": message,
+					"timestamp": now.strftime("%Y-%m-%d %H:%M")}    
+
+			print(comment)
+			comments.append(comment)
+			return True
